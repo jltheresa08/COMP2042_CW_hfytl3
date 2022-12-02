cd220c75224f42a3dcea153044c8a268d35ed6be
jltheresa08 <jltheresa08@gmail.com>
2022-12-03 01:04:23 +0800

    Added graphics

--- java/module-info.java
+++ java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.game.start to javafx.fxml;
     exports com.game.start;
