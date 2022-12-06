fb4a599befe939a23563caa8b17d93c95e746e44
jltheresa08 <jltheresa08@gmail.com>
2022-12-06 22:05:40 +0800

    Changed end game scene

--- java/com/game/end/EndGame.java
+++ java/com/game/end/EndGame.java
@@ -1,5 +1,10 @@
 package com.game.end;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import com.game.account.accountController;
+
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -9,106 +14,67 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.Optional;
-
-import com.game.account.Account;
-import com.game.account.accountController;
-
-
+/**
+ * This class loads the Game Over scene.
+ * 
+ * @author Theresa Lim -modified
+ */
 public class EndGame {
     private static EndGame singleInstance = null;
 
-    
+    /**
+     * Constructor for EndGame class.
+     */
     public EndGame(){
-    	
-    	
     }
     
+    /**
+     * This method creates a new EndGame object once.
+     * 
+     * @return EndGame object.
+     */
     public static EndGame getInstance(){
         if(singleInstance == null)
             singleInstance= new EndGame();
         return singleInstance;
     }
-
+    
+    /**
+     * This method loads the Game Over scene.
+     * 
+     * @param endGameScene Container for Game Over scene.
+     * @param root Group of components to be displayed on Game Over scene.
+     * @param primaryStage Container for a scene.
+     * @param score Current score of player.
+     */
     public void endGameShow(Scene endGameScene, Group root, Stage primaryStage,long score){
-        Text text = new Text("GAME OVER");
-        text.relocate(137,80);
-        text.setFont(Font.font(61));
-        root.getChildren().add(text);
-
-        //SCORE EDITING
-        accountController.score=score;
-        Text scoreText = new Text(score+"");
-        scoreText.setFill(Color.BLACK);
-        scoreText.relocate(250,200);
-        scoreText.setFont(Font.font(61));
-        root.getChildren().add(scoreText);
-        
-        Button saveButton = new Button("CHECK SCORE");
-        saveButton.setPrefSize(100,30);
-        root.getChildren().add(saveButton);
-        saveButton.relocate(250,250);
-        
-        saveButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-            	
-				try {
-					Parent root = FXMLLoader.load(getClass().getResource("/com/game/account/account.fxml"));
-					Stage window = (Stage)saveButton.getScene().getWindow();
-	            	window.setScene(new Scene(root));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-            	
-				
-            	/*Account newUser = new Account(name.getText());
-            	newUser.addToScore(score);
-            	Text notify = new Text("Saved!");
-            	notify.relocate(137,100);
-                notify.setFont(Font.font(61));
-                root.getChildren().add(notify);*/
-				
-            	//System.out.println("HELP");
-            }
-        });
-        
-        
-        
-        Button quitButton = new Button("QUIT");
-        quitButton.setPrefSize(100,30);
-        quitButton.setTextFill(Color.BLUE);
-        root.getChildren().add(quitButton);
-        //quitButton.setLayoutX(100);
-        //quitButton.setLayoutY(80);
-        quitButton.relocate(250,300);
-        quitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setTitle("Quit Dialog");
-                alert.setHeaderText("Quit from this page");
-                alert.setContentText("Are you sure?");
-
-                Optional<ButtonType> result = alert.showAndWait();
-                if (result.get() == ButtonType.OK){
-                    root.getChildren().clear();
-                    Platform.exit();
-                }
-            }
-        });
-
-
-
+    	
+    	
+    	endgameController.currentScore=score;
+    	accountController.score=score;
+    	
+    	Parent parentRoot;
+		try 
+		{
+			
+			parentRoot = FXMLLoader.load(getClass().getResource("endgame.fxml"));
+	        primaryStage.setScene(new Scene(parentRoot));
+	        primaryStage.setResizable(false);
+	        primaryStage.show();
+	    	
+		} 
+		catch (IOException e1) 
+		{
+			e1.printStackTrace();
+		}
+       
+    	
     }
 
 
---
+++ java/com/game/end/endgameController.java
@@ -0,0 +1,82 @@
+package com.game.end;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import com.game.account.accountController;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * This class is the controller for endgame.fxml.
+ * 
+ * @author Theresa Lim
+ */
+public class endgameController {
+
+	@FXML
+	private Button saveBtn, quitBtn;
+	
+	@FXML
+	private Label score;
+	
+	/**
+	 *  Get the current score.
+	 */
+	public static long currentScore = 0;
+	
+	/**
+	 * Display current score.
+	 */
+	public void initialize() {
+		score.setText(Long.toString(currentScore));
+	}
+		
+	/**
+	 * This method opens account page.
+	 * 
+	 * @param event Runs on SAVE button click.
+	 * @throws IOException if file not found.
+	 */
+	public void saveScore(ActionEvent event) throws IOException {
+		
+		Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+		Parent root = FXMLLoader.load(getClass().getResource("/com/game/account/account.fxml"));
+		stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+	}
+	
+	/**
+	 * This method closes the game window on button click.
+	 */
+	public void closeGame() {
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Quit Dialog");
+        alert.setHeaderText("Quit from this page");
+        alert.setContentText("Are you sure?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            Platform.exit();
+        }
+	}
+
+}
