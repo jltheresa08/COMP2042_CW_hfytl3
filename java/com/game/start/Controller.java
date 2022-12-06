b49057e23cf6a256eb8b537748b58ee95cafd54c
jltheresa08 <jltheresa08@gmail.com>
2022-12-06 22:09:41 +0800

    Changed name of sample.fxml to title.fxml.

--- java/com/game/start/Controller.java
+++ java/com/game/start/Controller.java
@@ -1,22 +1,18 @@
 package com.game.start;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -24,6 +20,11 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+/**
+ * This class is the controller for sample.fxml.
+ * 
+ * @author Theresa Lim - modified
+ */
 public class Controller implements Initializable {
 
 
@@ -41,32 +42,30 @@ public class Controller implements Initializable {
     
     
     private Stage primaryStage;
-    private Stage stage;
     private GameScene game;
     static final int WIDTH = 900;
     static final int HEIGHT = 900;
     private String[] mode = {"Default (4x4)", "5x5 Mode"};
     private Color currentColour = Color.rgb(189, 177, 92);
-    private boolean loggedin = false;
+
     
-    //@FXML
+    /**
+     * This method sets the stage and loads the scene.
+     * 
+     * @param event Runs on startBtn click.
+     */
     public void startGame(ActionEvent event){
     	this.primaryStage=(Stage)((Node)event.getSource()).getScene().getWindow();
-    	//startBtn.getScene().getWindow().hide();    	
-    	//FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.java"));  
-        //Stage stage = new Stage();
-        //stage.setScene(new Scene((Parent) loader.load()));
-        //stage.show();
-    	//Stage stage = new Stage();
     	changescene();
     	this.primaryStage.show();
 
     }
-    
+    /**
+     * This method loads the game scene.
+     */
     private void changescene(){
     	this.game = new GameScene();
     	
-    	 //Changes colour of game over scene
         Group endgameRoot = new Group();
         Scene endGameScene = new Scene(endgameRoot, 600, 400, Color.rgb(250, 20, 100, 0.2));
         
@@ -74,12 +73,15 @@ public class Controller implements Initializable {
         Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT, this.currentColour);//colour of game screen
         
          
-         this.game.game(gameScene, gameRoot, primaryStage, endGameScene, endgameRoot);//method in gamescene
-         this.primaryStage.setScene(gameScene);
+        this.game.game(gameScene, gameRoot, primaryStage, endGameScene, endgameRoot);//method in gamescene
+        this.primaryStage.setScene(gameScene);
          
     }
     
-    public void changeTheme(ActionEvent event) {
+    /**
+     * This method changes the colour of background of game.
+     */
+    public void changeTheme() {
     	this.currentColour = themeColour.getValue();
     	
     	BackgroundFill background_fill = new BackgroundFill(currentColour, CornerRadii.EMPTY, Insets.EMPTY);
@@ -90,9 +92,9 @@ public class Controller implements Initializable {
         
     }
     
-  
-    
-
+    /**
+     * This method initializes the values in choice box.
+     */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
@@ -101,6 +103,11 @@ public class Controller implements Initializable {
 		gameMode.setOnAction(this::changeMode);
 	}
     
+	/**
+	 * This method sets game mode based on choice box selection.
+	 * 
+	 * @param event On selection in choice box
+	 */
 	public void changeMode(ActionEvent event) {
 		String mode = gameMode.getValue();
 		
--- java/com/game/start/Main.java
+++ java/com/game/start/Main.java
@@ -2,149 +2,44 @@ package com.game.start;
 
 import javafx.application.Application;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.ButtonType;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.shape.Rectangle;
 
-import java.io.IOException;
-import java.util.Optional;
-import java.util.Scanner;
 
+/**
+ * Main class to start the application.
+ * 
+ * @author Theresa Lim - modified
+ */
 public class Main extends Application {
     static final int WIDTH = 900;
     static final int HEIGHT = 900;
-    private Group gameRoot = new Group();
-    private Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));
-    private static Scanner input= new Scanner(System.in);
     
-
-    public void setGameScene(Scene gameScene) {
-        this.gameScene = gameScene;
-    }
-
-    public void setGameRoot(Group gameRoot) {
-        this.gameRoot = gameRoot;
-    }
-    
-    
+	/**
+	 * This method loads the title scene of the game.
+	 * 
+	 * @param primaryStage An object of class Stage constructed by the platform, as the container of title scene.
+	 * @throws Exception When file not found
+	 */
     @Override
     public void start(Stage primaryStage) throws Exception {
     	
-    	/*FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("sample.fxml")); 
-        Parent parentRoot = loader.load();*/
-        
-    	Parent parentRoot = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    	Parent parentRoot = FXMLLoader.load(getClass().getResource("title.fxml"));
         primaryStage.setTitle("2048");
         primaryStage.setScene(new Scene(parentRoot));
         primaryStage.setResizable(false);
         primaryStage.show();
-        //primaryStage.showAndWait();
-        //Controller controller = loader.getController();
-       
-        Group accountRoot = new Group();
-        Scene accountScene = new Scene(accountRoot, WIDTH, HEIGHT, Color.rgb(150, 20, 100, 0.2));
-        
-        Group getAccountRoot = new Group();
-        Scene getAccountScene = new Scene(getAccountRoot, WIDTH, HEIGHT, Color.rgb(200, 20, 100, 0.2));
-        
-        /*//Changes colour of game over scene
-        Group endgameRoot = new Group();
-        Scene endGameScene = new Scene(endgameRoot, WIDTH, HEIGHT, Color.rgb(250, 20, 100, 0.2));
-        */
-        Group rankRoot = new Group();
-        Scene rankScene = new Scene(rankRoot, WIDTH, HEIGHT, Color.rgb(250, 50, 120, 0.3));
-        
-        /*BackgroundFill background_fill = new BackgroundFill(Color.rgb(120, 100, 100), CornerRadii.EMPTY, Insets.EMPTY);
-        Background background = new Background(background_fill);*/
-        
 
-        Group menuRoot = new Group();
-        Scene menuScene = new Scene(menuRoot, WIDTH, HEIGHT);
-        
-
-        Rectangle backgroundOfMenu = new Rectangle(240, 120, Color.rgb(120, 120, 120, 0.2));
-        backgroundOfMenu.setX(WIDTH / 2 - 120);
-        backgroundOfMenu.setY(180);
-        menuRoot.getChildren().add(backgroundOfMenu);
-
-        Rectangle backgroundOfMenuForPlay = new Rectangle(240, 140, Color.rgb(120, 20, 100, 0.2));
-        backgroundOfMenuForPlay.setX(WIDTH / 2 - 120);
-        backgroundOfMenuForPlay.setY(180);
-        accountRoot.getChildren().add(backgroundOfMenuForPlay);
-        
-        
-        /*Group gameRoot = new Group();
-        setGameRoot(gameRoot);
-        Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));//colour of game screen
-        setGameScene(gameScene);
-        primaryStage.setScene(gameScene);
-        GameScene game = new GameScene();
-        game.game(gameScene, gameRoot, primaryStage, endGameScene, endgameRoot);//method in gamescene
-        */
-        
-        //primaryStage.show();
     }
     
-    /*public void changeScene(){
-    	
-    	Stage stage = new Stage();
-    	
-    	Group menuRoot = new Group();
-        Scene menuScene = new Scene(menuRoot, WIDTH, HEIGHT);
-        
-        Group accountRoot = new Group();
-        Scene accountScene = new Scene(accountRoot, WIDTH, HEIGHT, Color.rgb(150, 20, 100, 0.2));
-        
-        Group getAccountRoot = new Group();
-        Scene getAccountScene = new Scene(getAccountRoot, WIDTH, HEIGHT, Color.rgb(200, 20, 100, 0.2));
-        
-        //Changes colour of game over scene
-        Group endgameRoot = new Group();
-        Scene endGameScene = new Scene(endgameRoot, WIDTH, HEIGHT, Color.rgb(250, 20, 100, 0.2));
-        
-        Group rankRoot = new Group();
-        Scene rankScene = new Scene(rankRoot, WIDTH, HEIGHT, Color.rgb(250, 50, 120, 0.3));
-        
-        BackgroundFill background_fill = new BackgroundFill(Color.rgb(120, 100, 100), CornerRadii.EMPTY, Insets.EMPTY);
-        Background background = new Background(background_fill);
-        
-        //The start
-        
-        Rectangle backgroundOfMenu = new Rectangle(240, 120, Color.rgb(120, 120, 120, 0.2));
-        backgroundOfMenu.setX(WIDTH / 2 - 120);
-        backgroundOfMenu.setY(180);
-        menuRoot.getChildren().add(backgroundOfMenu);
-        Rectangle backgroundOfMenuForPlay = new Rectangle(240, 140, Color.rgb(120, 20, 100, 0.2));
-        backgroundOfMenuForPlay.setX(WIDTH / 2 - 120);
-        backgroundOfMenuForPlay.setY(180);
-        accountRoot.getChildren().add(backgroundOfMenuForPlay);
-        
-        
-        Group gameRoot = new Group();
-        setGameRoot(gameRoot);
-        Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));//colour of game screen
-        setGameScene(gameScene);
-        stage.setScene(gameScene);
-        GameScene game = new GameScene();
-        game.game(gameScene, gameRoot, stage, endGameScene, endgameRoot);//method in gamescene
-        
-        stage.show();
-    }
-    */
     
-    
+    /**
+     * This method launches the application.
+     * 
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         launch(args);
     }
