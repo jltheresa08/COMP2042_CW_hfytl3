76472f6a58c68332a26229a19c81b57e3a5f36d6
jltheresa08 <jltheresa08@gmail.com>
2022-12-11 21:29:37 +0800

    Added differentiation between whether player has saved score before.

--- java/com/game/account/Account.java
+++ java/com/game/account/Account.java
@@ -1,6 +1,7 @@
 package com.game.account;
 
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,43 +13,65 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+
+/**
+ * Object of this class represents player's account
+ * 
+ * @author Theresa Lim - modified
+ */
 public class Account implements Comparable<Account> {
-	
-    private  long score = 0;
+    private long score = 0;
 	private String userName;
     private Account account;
-    private File file = new File("test.txt");
-    public static boolean highscoreChanged;
+    private String fileName = "accounts.txt";
+    private File file;
     private static ArrayList<Account> accounts = new ArrayList<>();
+    
+	/**
+	 * Check if player has saved score before.
+	 */  
+    public static boolean newPlayer;
 
+    /**
+     * This constructor saves player's user name and score.
+     * 
+     * @param userName Player's user name.
+     * @param score Player's score of current round.
+     */
     public Account(String userName, long score) {
 	    	
     	this.userName=userName;
 	   	this.score=score;
 
-   }
+    }
     
-    
+    /**
+     * This constructor runs through process of checking and saving player's user name and score.
+     * Accounts previously saved are added into empty ArrayList.
+     * ArrayList is cleared and re-added with new list after saving new details.
+     */
     public Account(){
     	
     	this(accountController.name,accountController.score);
     	
-    	try
+    	setFileName(fileName);
+    	
+    	try 
     	{
-    		accounts.clear();
+			createFile(file);
+			//accounts.clear();
 			getUserList();
-		}
-    	catch (FileNotFoundException e)
+		} 
+    	catch (IOException e) 
     	{
+			
 			e.printStackTrace();
 		}
-
     	
-    	//read file to get list of registered usernames
-
         if(accountHaveBeenExist(userName)==null)
         {
         	account = makeNewAccount(userName);
+        	newPlayer = true;
         	
         	try 
         	{
@@ -63,6 +86,7 @@ public class Account implements Comparable<Account> {
         else 
         {
         	account = accountHaveBeenExist(userName);
+        	newPlayer = false;
         	
         	try
         	{
@@ -77,7 +101,7 @@ public class Account implements Comparable<Account> {
         
         try
     	{
-    		accounts.clear();
+        	//accounts.clear();
 			getUserList();
 		}
     	catch (FileNotFoundException e)
@@ -85,44 +109,88 @@ public class Account implements Comparable<Account> {
 			e.printStackTrace();
 		}
         
-        getHighScore();
+        sortByScore();
     }
     
     
-
+    /**
+     * This method compares high scores of all player's saved.
+     * 
+     * @param user Current Player's account.
+     * @return the higher score.
+     */
     @Override
-    public int compareTo(Account o) {
-        return Long.compare(o.getScore(), score);
+    public int compareTo(Account user) {
+        return Long.compare(user.getScore(), score);
     }
     
+    /**
+     * This method represents Account objects in String.
+     * @return Player's user name and score in string.
+     */
     @Override
     public String toString() {
     	return userName + "," +score;
     }
     
+    /**
+     * This method creates a file to store accounts.
+     * 
+     * @param file Name and type of file.
+     * @throws IOException if creation of file has error
+     */
+    public void createFile(File file) throws IOException {
+
+	    if (file.createNewFile())
+	    {
+	    	System.out.println("File created: " + file.getName());
+	    } 
+	    else
+	    {
+	    	System.out.println("File already exists.");
+	    }
+    }
     
-    
-    private void addUser(Account account) throws IOException {
-    	//File file = new File("test.txt");
+    /**
+     * This method saves an account into a text file.
+     * @param account Current player's account.
+     * @throws IOException If file not found.
+     */
+    public void addUser(Account account) throws IOException {
+    	
     	FileWriter fileWriter = new FileWriter(file, true);
     	
-    	fileWriter.write(account.toString());
+    	fileWriter.write(account.toString()+"\n");
         fileWriter.close();
     }
     
- 
-    private long getScore() {
+    public long getScore() {
         return score;
     }
-
-    private String getUserName() {
+    
+    public String getUserName() {
         return userName;
     }
     
-    private void getUserList() throws FileNotFoundException {
+    /**
+     * This method sets file to be created.
+     * 
+     * @param fileName Name of file to be created.
+     */
+    public void setFileName(String fileName) {
+    	file = new File(fileName);
+    	this.fileName = fileName;
+    }
+    
+    /**
+     * This method reads all previous account from file and adds into accounts ArrayList.
+     * 
+     * @throws FileNotFoundException If file to open isn't found.
+     */
+    public void getUserList() throws FileNotFoundException {
     	
+    	accounts.clear();
     	String[] userNscore;
-    	//File myObj = new File("test.txt");
 		Scanner getUserList = new Scanner(file);
 		
 		while(getUserList.hasNextLine())
@@ -132,11 +200,18 @@ public class Account implements Comparable<Account> {
 			accounts.add(new Account(userNscore[0],Long.parseLong(userNscore[1])));
 		}
     }
-    private void updateScore(Account account) throws IOException {
+    
+    /**
+     * This method overwrites current score of account in file if a new high score is achieved.
+     * 
+     * @param account Current player's account.
+     * @throws IOException If file not found.
+     */
+    public void updateScore(Account account) throws IOException {
     	
-    	Path path = Paths.get("test.txt");
+    	Path path = Paths.get(fileName);
         List<String> lines = Files.readAllLines(path);
-        
+
         if(compareScore(account))
         {
         	lines.set((accounts.indexOf(account)), (userName+","+score));
@@ -146,32 +221,37 @@ public class Account implements Comparable<Account> {
         	System.out.println("No change");
     }
     
-    private boolean compareScore(Account account){
+    /**
+     * This method checks if current score is higher than previous highest score.
+     * 
+     * @param account Current player's account.
+     * @return True current score is higher, false if otherwise.
+     */
+    public boolean compareScore(Account account){
     	
     	long prevScore = account.getScore();
         
         if(score>prevScore)
-        {
-        	highscoreChanged = true;
         	return true;
-        }
-        	
         else
-    	{
-        	highscoreChanged = false;
         	return false;
-    	}
     }
-    
-    private void getHighScore() {
+
+    /**
+     * This method sorts all saved accounts from highest score to lowest.
+     */
+    public void sortByScore() {
     	
     	Collections.sort(accounts);
-    	
-    	System.out.println(accounts);
-    
-    
     }
-    private Account accountHaveBeenExist(String userName) {
+    
+    /**
+     * This method checks if the user name entered has been saved before.
+     * 
+     * @param userName Player's account name.
+     * @return account if user name was found, null otherwise.
+     */
+    public Account accountHaveBeenExist(String userName) {
     	
     	for(Account account : accounts)
     	{
@@ -185,7 +265,13 @@ public class Account implements Comparable<Account> {
 
     }
 
-    private Account makeNewAccount(String userName){
+    /**
+     * This method adds current user name into accounts ArrayList.
+     * 
+     * @param userName Player's account name.
+     * @return account added into ArrayList.
+     */
+    public Account makeNewAccount(String userName){
     	
         Account account = new Account(userName,score);
         accounts.add(account);
--- java/com/game/account/accountController.java
+++ java/com/game/account/accountController.java
@@ -1,26 +1,26 @@
 package com.game.account;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Optional;
 
-import com.game.end.EndGame;
-import com.game.start.Controller;
-import com.game.start.Main;
-
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
+
+/**
+ * This class is the controller for account.fxml.
+ * 
+ * @author Theresa Lim
+ */
 public class accountController{
 
     @FXML
-    private Button enterBtn;
+    private Button enterBtn, quitBtn;
     
     @FXML
     private TextField userName;
@@ -28,25 +28,55 @@ public class accountController{
     @FXML
     private Label userBest;
     
-    
+    /**
+     * Get the current score.
+     */
     public static long score=0;
+    
+    /**
+     * Get user name of current player.
+     */
     public static String name;
     
     //static because need to be accessed by EndGame.java
     
-    public void loginUser(ActionEvent event) throws IOException {
+    /**
+     * This method notifies when new high score achieved.
+     * High Score is compared in Account.java.
+     * @see Account
+     * 
+     */
+    public void loginUser() {
     	
     	name = userName.getText();
-   
-    	new Account();
-    	
-    	if(Account.highscoreChanged)
-    	{
+
+    	Account currentUser = new Account();
+
+    	if(currentUser.compareScore(currentUser))
     		userBest.setText("New personal best!");
+    	else
+    	{
+    		if(Account.newPlayer)
+    			userBest.setText("Score is saved!");
+    		else
+    			userBest.setText("No change in score!");
     	}
-    	
-    	
+    		
     }
 
+    /**
+	 * This method closes the game window on button click.
+	 */
+    public void closeGame() {
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Quit Dialog");
+        alert.setHeaderText("Quit from this page");
+        alert.setContentText("Are you sure?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            Platform.exit();
+        }
+	}
     
 }
